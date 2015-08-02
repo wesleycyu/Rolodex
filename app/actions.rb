15416c23f2ba86c826057b3b7a42f3f68873842e
Wes <wesley.ch.yu@gmail.com>
2015-08-02 14:21:57 -0700

    Create Update and Delete Routes for Contacts

--- app/actions.rb
+++ app/actions.rb
@@ -1,21 +1,40 @@
-# Homepage (Root path)
+# Show All Contacts
 get '/' do
   @contacts = Contact.all
   erb :index
 end
 
+# Query Contact Database
 get '/contacts' do
   @contact = Contact.where("lower(firstname || lastname || email) LIKE ?", "%#{params[:query].downcase}%").order(:firstname)
   @contact.to_json
 end
 
+# Create Contact
 post '/contacts' do
   puts params[:firstname]
   @contact = Contact.create({firstname: params[:firstname], lastname: params[:lastname], email: params[:email]})
   @contact.to_json
 end
 
+# Find Conact
 get '/contact/:id' do
   @contact = Contact.find(params[:id])
   @contact.to_json
+end
+
+# Update Contact
+put '/api/:id' do
+  contact = Contact.find(params[:id])
+    contact.update(
+    name:   params[:name],
+    email:  params[:email],
+    phone: params[:phone]
+    )
+end
+
+# Delete Contact
+delete '/api/:id' do
+  contact = Contact.find(params[:id])
+  contact.destroy
 end
